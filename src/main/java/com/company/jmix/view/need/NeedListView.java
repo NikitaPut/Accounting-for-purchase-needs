2d4d403bc51fd384c3063d9dd80421cd579e02cc
HOME-PC\Skorpion <nikitaputintsev@gmail.com>
2025-06-20 10:03:18 +0300

    Resolved merge conflict in NeedListView and NeedDetailView

--- src/main/java/com/company/jmix/view/need/NeedListView.java
+++ src/main/java/com/company/jmix/view/need/NeedListView.java
@@ -1,34 +1,26 @@
 package com.company.jmix.view.need;
 
 import com.company.jmix.entity.Need;
-import com.company.jmix.entity.NeedPeriod;
 import com.company.jmix.entity.NeedCategory;
+import com.company.jmix.entity.NeedPeriod;
 import com.company.jmix.service.NeedCalculationService;
 import com.company.jmix.view.main.MainView;
 import com.company.jmix.view.needdetail.NeedDetailView;
-<<<<<<< HEAD
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
-=======
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
 import com.vaadin.flow.router.Route;
 import io.jmix.core.DataManager;
 import io.jmix.core.FetchPlan;
 import io.jmix.core.Messages;
-<<<<<<< HEAD
 import io.jmix.flowui.DialogWindows;
-=======
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.grid.DataGrid;
-import io.jmix.flowui.component.combobox.JmixComboBox;
 import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,39 +47,28 @@ public class NeedListView extends StandardListView<Need> {
     @Autowired
     private Messages messages;
     @Autowired
-<<<<<<< HEAD
     private DialogWindows dialogWindows;
-=======
-    private DialogWindow dialogWindowManager;
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
 
     @Subscribe
     public void onInit(InitEvent event) {
-        // Начальная загрузка данных
         needsDl.load();
     }
 
-<<<<<<< HEAD
-    @Subscribe("createBtn") // ID кнопки создания из XML
+    @Subscribe("createBtn")
     public void onCreateBtnClick(ClickEvent<Button> event) {
-        // Создаем новую сущность
         Need newNeed = dataManager.create(Need.class);
 
-        // Открываем диалоговое окно редактирования
         DialogWindow<NeedDetailView> dialog = dialogWindows.detail(this, Need.class)
                 .editEntity(newNeed)
                 .withViewClass(NeedDetailView.class)
                 .build();
 
         NeedDetailView view = dialog.getView();
-        view.setDialogWindow(dialog); // Передаем ссылку на диалог
+        view.setDialogWindow(dialog);
 
         dialog.open();
     }
 
-
-=======
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
     @Subscribe("generateTotalAction")
     public void onGenerateTotal(ActionPerformedEvent event) {
         Need selected = needsDataGrid.getSingleSelectedItem();
@@ -141,7 +122,6 @@ public class NeedListView extends StandardListView<Need> {
         Need newNeed = dataManager.create(Need.class);
         newNeed.setPeriod(lastPeriod.get());
 
-        // Предварительно загружаем данные для диалога
         List<NeedPeriod> periods = dataManager.load(NeedPeriod.class)
                 .query("select p from NeedPeriod p order by p.id desc")
                 .fetchPlan(FetchPlan.INSTANCE_NAME)
@@ -160,9 +140,6 @@ public class NeedListView extends StandardListView<Need> {
         }
 
         try {
-//            ExcelExporter excelExporter = applicationContext.getBean(ExcelExporter.class);
-//            excelExporter.setFileName("Needs_" + LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm")) + ".xlsx");
-//            excelExporter.exportDataGrid(needsDataGrid);
             notifications.show(messages.getMessage("need.exportSuccess"));
         } catch (Exception e) {
             notifications.show(messages.formatMessage("need.exportError", e.getMessage()));
--- src/main/java/com/company/jmix/view/needdetail/NeedDetailView.java
+++ src/main/java/com/company/jmix/view/needdetail/NeedDetailView.java
@@ -1,24 +1,16 @@
 package com.company.jmix.view.needdetail;
 
-<<<<<<< HEAD
-import com.company.jmix.entity.*;
+import com.company.jmix.entity.Need;
+import com.company.jmix.entity.NeedPeriod;
+import com.company.jmix.entity.NeedCategory;
 import com.company.jmix.view.main.MainView;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.DataManager;
-import io.jmix.flowui.model.*;
-=======
-import com.company.jmix.entity.Need;
-import com.company.jmix.entity.NeedPeriod;
-import com.company.jmix.entity.NeedCategory;
-import com.company.jmix.view.main.MainView;
-import com.vaadin.flow.router.Route;
-import io.jmix.core.DataManager;
 import io.jmix.core.FetchPlan;
 import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.InstanceContainer;
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -30,21 +22,15 @@ import java.util.Optional;
 @Route(value = "need-detail-list", layout = MainView.class)
 public class NeedDetailView extends StandardDetailView<Need> {
 
-<<<<<<< HEAD
     private DialogWindow<NeedDetailView> dialogWindow;
 
-=======
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
     @ViewComponent
     private InstanceContainer<Need> needDc;
 
     @ViewComponent
-<<<<<<< HEAD
-=======
     private CollectionContainer<NeedPeriod> periodsDc;
 
     @ViewComponent
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
     private CollectionContainer<NeedCategory> categoriesDc;
 
     @Autowired
@@ -52,7 +38,7 @@ public class NeedDetailView extends StandardDetailView<Need> {
 
     @Subscribe
     public void onInit(InitEvent event) {
-<<<<<<< HEAD
+        loadPeriods();
         loadCategories();
     }
 
@@ -65,29 +51,17 @@ public class NeedDetailView extends StandardDetailView<Need> {
         this.dialogWindow = dialogWindow;
     }
 
-    private void loadCategories() {
-        categoriesDc.setItems(dataManager.load(NeedCategory.class)
-                .query("select c from NeedCategory c order by c.name")
-=======
-        // Загрузка данных с предопределенным fetchPlan
-        loadPeriods();
-        loadCategories();
-    }
-
     private void loadPeriods() {
-        // Используем предопределенный fetchPlan _instance_name, который включает базовые атрибуты
         periodsDc.setItems(dataManager.load(NeedPeriod.class)
                 .query("select p from NeedPeriod p order by p.id desc")
-                .fetchPlan(FetchPlan.INSTANCE_NAME) // _instance_name включает name
+                .fetchPlan(FetchPlan.INSTANCE_NAME)
                 .list());
     }
 
     private void loadCategories() {
-        // Используем предопределенный fetchPlan _instance_name, который включает name
         categoriesDc.setItems(dataManager.load(NeedCategory.class)
                 .query("select c from NeedCategory c order by c.name")
-                .fetchPlan(FetchPlan.INSTANCE_NAME) // _instance_name включает name
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
+                .fetchPlan(FetchPlan.INSTANCE_NAME)
                 .list());
     }
 
@@ -104,58 +78,22 @@ public class NeedDetailView extends StandardDetailView<Need> {
         need.setApproved(false);
         need.setAccounted(false);
         need.setIsTotal(false);
-
-        // Установка последнего периода
         setLatestPeriod(need);
     }
 
     private void setLatestPeriod(Need need) {
-<<<<<<< HEAD
-        dataManager.load(NeedPeriod.class)
-                .query("select p from NeedPeriod p order by p.id desc")
-                .maxResults(1)
-                .optional()
-                .ifPresent(need::setPeriod);
-=======
-        // Загружаем с предопределенным fetchPlan
         Optional<NeedPeriod> latestPeriod = dataManager.load(NeedPeriod.class)
                 .query("select p from NeedPeriod p order by p.id desc")
                 .fetchPlan(FetchPlan.INSTANCE_NAME)
                 .optional();
 
         latestPeriod.ifPresent(need::setPeriod);
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
     }
 
     @Subscribe
     public void onValidation(ValidationEvent event) {
         Need need = getEditedEntity();
 
-<<<<<<< HEAD
-        validateQuantity(need, event);
-        validatePeriod(need, event);
-        validateCategory(need, event);
-    }
-
-    private void validateQuantity(Need need, ValidationEvent event) {
-        if (need.getQuantity() != null && need.getQuantity() <= 0) {
-            event.getErrors().add("Количество должно быть положительным");
-        }
-    }
-
-    private void validatePeriod(Need need, ValidationEvent event) {
-        if (need.getPeriod() == null) {
-            event.getErrors().add("Необходимо указать период");
-        }
-    }
-
-    private void validateCategory(Need need, ValidationEvent event) {
-        if (need.getCategory() == null) {
-            event.getErrors().add("Необходимо указать категорию");
-        }
-    }
-}
-=======
         if (need.getQuantity() != null && need.getQuantity() <= 0) {
             event.getErrors().add("Количество должно быть положительным");
         }
@@ -167,4 +105,3 @@ public class NeedDetailView extends StandardDetailView<Need> {
         }
     }
 }
->>>>>>> 59c5684a9f8e20cd85a76c94f451d5f52e5a3e6e
